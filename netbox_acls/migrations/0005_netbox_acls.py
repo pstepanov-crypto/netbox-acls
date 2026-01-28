af4a5b4c00495b38cd68af2c8b59663258170fee
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2026-01-28 13:02:27 +0300

    Update 0005_netbox_acls.py

--- netbox_acls/migrations/0005_netbox_acls.py
+++ netbox_acls/migrations/0005_netbox_acls.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # ТОЛЬКО добавляем новые CharField поля
+        # Добавляем новые CharField поля для префиксов
         migrations.AddField(
             model_name='aclextendedrule',
             name='destination_prefix',
@@ -37,6 +37,27 @@ class Migration(migrations.Migration):
                 max_length=100,
                 verbose_name='Source Prefix/Host',
                 help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)'
+            ),
+        ),
+        # Добавляем новые поля портов как CharField
+        migrations.AddField(
+            model_name='aclextendedrule',
+            name='destination_ports',
+            field=models.CharField(
+                blank=True,
+                max_length=100,
+                verbose_name='Destination Ports',
+                help_text='Port numbers or ranges (e.g., 80, 443, 1000-2000, eq www, range 445 1050)',
+            ),
+        ),
+        migrations.AddField(
+            model_name='aclextendedrule',
+            name='source_ports',
+            field=models.CharField(
+                blank=True,
+                max_length=100,
+                verbose_name='Source Ports',
+                help_text='Port numbers or ranges (e.g., 80, 443, 1000-2000, eq www, range 445 1050)',
             ),
         ),
     ]
