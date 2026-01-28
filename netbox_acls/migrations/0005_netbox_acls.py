9e4bd208857eb74438bbe05d09b7f5f60e1b2a17
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2026-01-28 13:12:23 +0300

    Update 0005_netbox_acls.py

--- netbox_acls/migrations/0005_netbox_acls.py
+++ netbox_acls/migrations/0005_netbox_acls.py
@@ -1,4 +1,4 @@
-# netbox_acls/migrations/0005_fix_prefix_fields.py
+# netbox_acls/migrations/0005_change_all_field_types.py
 from django.db import migrations, models
 
 
@@ -8,39 +8,39 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # Добавляем новые CharField поля для префиксов
-        migrations.AddField(
+        # Изменяем тип полей префиксов
+        migrations.AlterField(
             model_name='aclextendedrule',
             name='destination_prefix',
             field=models.CharField(
                 blank=True,
                 max_length=100,
                 verbose_name='Destination Prefix/Host',
-                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)'
+                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)',
             ),
         ),
-        migrations.AddField(
+        migrations.AlterField(
             model_name='aclextendedrule',
             name='source_prefix',
             field=models.CharField(
                 blank=True,
                 max_length=100,
                 verbose_name='Source Prefix/Host',
-                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)'
+                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)',
             ),
         ),
-        migrations.AddField(
+        migrations.AlterField(
             model_name='aclstandardrule',
             name='source_prefix',
             field=models.CharField(
                 blank=True,
                 max_length=100,
                 verbose_name='Source Prefix/Host',
-                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)'
+                help_text='IP prefix, network or host (e.g., 192.168.1.0/24, 192.168.1.0 255.255.255.0, host 10.1.1.1)',
             ),
         ),
-        # Добавляем новые поля портов как CharField
-        migrations.AddField(
+        # Изменяем тип полей портов
+        migrations.AlterField(
             model_name='aclextendedrule',
             name='destination_ports',
             field=models.CharField(
@@ -50,7 +50,7 @@ class Migration(migrations.Migration):
                 help_text='Port numbers or ranges (e.g., 80, 443, 1000-2000, eq www, range 445 1050)',
             ),
         ),
-        migrations.AddField(
+        migrations.AlterField(
             model_name='aclextendedrule',
             name='source_ports',
             field=models.CharField(
